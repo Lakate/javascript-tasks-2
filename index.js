ed69383ca85dc088c7f20b3cbd511a5e5c726956
Kate Latypova <katelat3009@gmail.com>
2015-10-08 14:45:22 +0500

    initial version

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 'use strict';
 
-var phoneBook = require('./phoneBook');
+var phoneBook = require('./phone-book');
+var countPerson = 0;
 
 // Добавляем записи
-phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
-phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
-phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+countPerson += phoneBook.add('Сергей', '7 (999) 666-7-778', 'gs@example.com');
+countPerson += phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
+countPerson += phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+countPerson += phoneBook.add('Екатерина', '+79991234567', 'lakate@yandex-team.ru');
+countPerson += phoneBook.add('Павел', '+7 999 897 6 453', 'bratva@yandex-team.ru');
+countPerson += phoneBook.add('Руслан', '+34 (999) 313-7-212', 'just713@yandex-team.ru');
 
 // Невалидные данные не должны попадать в книгу!
-phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
+countPerson += phoneBook.add('Честный Хрюндель', '7+ (111) 777-2-222', 'info@example.com.ru');
+countPerson += phoneBook.add('Бесчестный Хрендель', '7 (111) 777-2-222', 'infoexample.com.ru');
 
+console.log(countPerson.toString() + ' person in phonebook\n');
+
+console.log('Query: ' + '777');
 phoneBook.find('777');
+console.log('\n');
 // Выводит построчно записи, все поля через запятую:
 // Сергей, +7 (999) 666-7-778, gogolef@yandex-team.ru
 // Олег, +7 (999) 777-7-777, just7@yandex-team.ru
+console.log('Query: ' + 'Сергей');
+phoneBook.find('Сергей');
+console.log('\n');
+// Сергей, 7 (999) 666-7-778, gs@example.com
+// Сергей 2, 999 4433444, gs@example.com
 
-phoneBook.remove('Олег');
+countPerson -= phoneBook.remove('Олег');
+console.log(countPerson.toString() + ' person in phonebook\n');
 // Выводит количество удалённых контактов, которые удовлетворят запросу:
 // Удален 1 контакт
 
+// Экспортируем записи, пример файла рядом
+phoneBook.importFromCsv('./backup.csv');
+// Добавлено 4 контакта
+
 // Выводит записи в виде красивой таблички
 phoneBook.showTable();
-// Выводит
-// ┌─────────────┬────────────────────╥──────────────────┐
-// │ Имя         │ Телефон            ║ email            │
-// ├─────────────┼────────────────────╫──────────────────┤
-// │ Сергей      │ +7 (999) 666-77-78 ║ gs@example.com   │
-// │ Сергей 2    │ +7 (999) 443-34-44 ║ gs@example.com   │
-// └─────────────┴────────────────────╨──────────────────┘
-
-
-// Экспортируем записи, пример файла рядом
-phoneBook.export('./backup.csv');
-// Добавлено 4 контакта
--- phone-book.js
+++ phone-book.js
@@ -1,52 +1,244 @@
 'use strict';
 
-var phoneBook; // Здесь вы храните записи как хотите
+var phoneBook = [];
 
 /*
    Функция добавления записи в телефонную книгу.
    На вход может прийти что угодно, будьте осторожны.
 */
 module.exports.add = function add(name, phone, email) {
-
-    // Ваша невероятная магия здесь
-
+    var isAdd = 0;
+    if (isValidPhone(phone) && isValidEmail(email)) {
+        var newPerson = {
+            name: name,
+            phone: phone.replace(/\+|\s|-|\(|\)/g, ''),
+            email: email
+        };
+        phoneBook.push(newPerson);
+        isAdd++;
+    }
+    return isAdd;
 };
+
+/*
+    Функция, определяющая валидность телефонного номера
+*/
+function isValidPhone(phone) {
+    var rePhone = /^(\+?\d{1,2})?\s?(\(\d{3}\)|(\d{3}))\s?(\d{3})(\s|-)?(\d{1})(\s|-)?(\d{3})$/;
+    return rePhone.exec(phone) === null ? false : true;
+}
+
+/*
+ Функция, определяющая валидность email
+ */
+function isValidEmail(email) {
+    var reEmail = /^(\w)+@[A-Za-zА-Яа-я0-9_-]+\.[A-Za-zА-Яа-я0-9_]+(\.([A-Za-zА-Яа-я0-9_])+)?/;
+    return reEmail.exec(email) === null ? false : true;
+}
 
 /*
    Функция поиска записи в телефонную книгу.
    Поиск ведется по всем полям.
 */
 module.exports.find = function find(query) {
+    if (query === null) {
+        for (var i = 0; i < phoneBook.length; i++) {
+            console.log(phoneBook[i].name + ', ' + phoneBook[i].phone + ', ' + phoneBook[i].email);
+        }
+    } else {
+        for (var i = 0; i < phoneBook.length; i++) {
+            if (phoneBook[i].name.indexOf(query) != -1 ||
+                phoneBook[i].phone.indexOf(query) != -1 ||
+                phoneBook[i].email.indexOf(query) != -1) {
+                console.log(phoneBook[i].name + ', ' + getCorrectPhone(phoneBook[i].phone) +
+                            ', ' + phoneBook[i].email);
 
-    // Ваша удивительная магия здесь
-
+            }
+        }
+    }
 };
 
 /*
    Функция удаления записи в телефонной книге.
 */
 module.exports.remove = function remove(query) {
-
-    // Ваша необьяснимая магия здесь
-
+    var countDel = 0;
+    for (var i = 0; i < phoneBook.length; i++) {
+        if (phoneBook[i].name.localeCompare(query) == 0) {
+            phoneBook.splice(i, 1);
+            countDel++;
+        }
+    }
+    console.log(countDel.toString() + ' contacts deleted');
+    return countDel;
 };
 
 /*
    Функция импорта записей из файла (задача со звёздочкой!).
 */
 module.exports.importFromCsv = function importFromCsv(filename) {
-    var data = require('fs').readFileSync(filename, 'utf-8');
+    var data = require('fs').readFileSync(filename, 'utf-8').split('\n');
+    var temp;
+    var countAddPerson = 0;
 
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
+    for (var i = 0; i < data.length; i++) {
+        temp = data[i].split(';');
+        countAddPerson += module.exports.add(temp[0], temp[1], temp[2]);
+    }
+    console.log('Add ' + countAddPerson.toString() + ' person');
 };
 
 /*
    Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
 */
 module.exports.showTable = function showTable(filename) {
+    var table = {
+        leftUpCorner: '╔',
+        rightUpCorner: '╗',
+        leftDownCorner: '╚',
+        rightDownCorner: '╝',
+        fatHorizontal: '═',
+        fatVertical: '║',
+        fatToDown: '╦',
+        fatToUp: '╩',
+        fatCrossroad: '╫',
+        thinCrossroad: '┼',
+        thinHorizontal: '─',
+        thinVertical: '│',
+        leftThinHorizontal: '╟',
+        rightThinHorizontal: '╢',
+        thinToDown: '╤',
+        thinToUp: '╧'
+    };
 
-    // Ваша чёрная магия здесь
+    var nameWidth = 0;
+    var phoneWidth = 22;
+    var emailWidth = 0;
+    for (var i = 0; i < phoneBook.length; i++) {
+        nameWidth = max(nameWidth, phoneBook[i].name.length);
+        emailWidth = max(emailWidth, phoneBook[i].email.length);
+    }
+    nameWidth += 2;
+    emailWidth += 2;
 
+    var frame = getFrame(table, nameWidth, phoneWidth, emailWidth);
+    console.log(frame[0]);
+
+    var phoneTable = table.fatVertical;
+    for (var i = 0; i < phoneBook.length; i++) {
+        console.log(frame[2]);
+        for (var j = 0; j < nameWidth + phoneWidth + emailWidth + 7; j++) {
+            switch (j) {
+                case 1:
+                    phoneTable += phoneBook[i].name;
+                    for (var k = 0; k < nameWidth - phoneBook[i].name.length; k++) {
+                        phoneTable += ' ';
+                    }
+                    break;
+                case nameWidth + 4:
+                    var correctPhone = getCorrectPhone(phoneBook[i].phone);
+                    phoneTable += correctPhone;
+                    for (var k = 0; k < phoneWidth - correctPhone.length - 3; k++) {
+                        phoneTable += ' ';
+                    }
+                    break;
+                case nameWidth + phoneWidth + 7:
+                    phoneTable += phoneBook[i].email;
+                    for (var k = 0; k < emailWidth - phoneBook[i].email.length; k++) {
+                        phoneTable += ' ';
+                    }
+                    break;
+                case nameWidth + 2:
+                    phoneTable += table.thinVertical;
+                    break;
+                case nameWidth + phoneWidth + 5:
+                    phoneTable += table.fatVertical;
+                    break;
+                case 0:
+                    phoneTable += '  ';
+                    break;
+                case nameWidth + 3:
+                    phoneTable += '  ';
+                    break;
+                case nameWidth + phoneWidth + 4 :
+                    phoneTable += '  ';
+                    break;
+                case nameWidth + phoneWidth + 6:
+                    phoneTable += '  ';
+                    break;
+                case nameWidth + phoneWidth + emailWidth + 7:
+                    phoneTable += '  ';
+                    break;
+            }
+        }
+        console.log(phoneTable + ' ' + table.fatVertical);
+        phoneTable = table.fatVertical;
+        console.log(frame[2]);
+        if (i == phoneBook.length - 1) {
+            console.log(frame[3]);
+        } else {
+            console.log(frame[1]);
+        }
+    }
 };
+
+/*
+    Функция нахождения максимума
+*/
+function max(a, b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+/*
+    Функция строит остов таблицы - т.е. те строки, которые в ней повторяются.
+*/
+function getFrame(table, nameWidth, phoneWidth, emailWidth) {
+    var frame = [table.leftUpCorner, table.leftThinHorizontal,
+                 table.fatVertical, table.leftDownCorner];
+    for (var i = 0; i < nameWidth + phoneWidth + emailWidth + 7; i++) {
+        if (i == nameWidth + 3) {
+            frame[0] += table.thinToDown;
+            frame[1] += table.thinCrossroad;
+            frame[2] += table.thinVertical;
+            frame[3] += table.thinToUp;
+        }
+        if (i == nameWidth + phoneWidth + 4) {
+            frame[0] += table.fatToDown;
+            frame[1] += table.fatCrossroad;
+            frame[2] += table.fatVertical;
+            frame[3] += table.fatToUp;
+        }
+        if (i <= nameWidth + 1 ||
+            i > nameWidth + 2 && i <= nameWidth + phoneWidth + 2 ||
+            i > nameWidth + phoneWidth) {
+            frame[0] += table.fatHorizontal;
+            frame[1] += table.thinHorizontal;
+            frame[2] += ' ';
+            frame[3] += table.fatHorizontal;
+        }
+    }
+    frame[0] += table.rightUpCorner;
+    frame[1] += table.rightThinHorizontal;
+    frame[2] += table.fatVertical;
+    frame[3] += table.rightDownCorner;
+
+    return frame;
+}
+
+/*
+    Функция делает телефонный номер красивым, для вывода на экран
+*/
+function getCorrectPhone(phone) {
+    var correctPhone = '(' + phone.substr(phone.length - 10, 3) + ') ' +
+                        phone.substr(phone.length - 7, 3) + '-' + phone[phone.length - 4] +
+                        '-' + phone.substr(phone.length - 3, 3);
+    correctPhone = phone.length == 10 ?
+                   '+7 ' + correctPhone : phone.substr(0, phone.length - 10) + ' ' + correctPhone;
+    if (correctPhone[0] != '+') {
+        correctPhone = phone.length == 10 ? '+7 ' + correctPhone : '+' + correctPhone;
+    }
+    return correctPhone;
+}
